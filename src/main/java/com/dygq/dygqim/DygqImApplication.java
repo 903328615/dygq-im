692bf5b814b137d7fa590635b51e7d35f436755f
wangzibin <wangzibin@kanzhun.com>
2021-04-13 22:53:02 +0800

    init obj

---
+++ src/main/java/com/dygq/dygqim/DygqImApplication.java
@@ -0,0 +1,13 @@
+package com.dygq.dygqim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DygqImApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DygqImApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dygq/dygqim/DygqImApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dygq.dygqim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DygqImApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
